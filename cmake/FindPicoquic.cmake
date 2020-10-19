5d9af8965aaa6dbcb100075410695f2b2ce4749f
Christian Huitema <huitema@huitema.net>
2020-10-19 12:59:55 -0700

    Try to find autoqlog.h

--- cmake/FindPicoquic.cmake
+++ cmake/FindPicoquic.cmake
@@ -1,14 +1,22 @@
 # - Try to find Picoquic
 
+
 find_path(Picoquic_INCLUDE_DIR
     NAMES picoquic.h
     HINTS ${CMAKE_SOURCE_DIR}/../picoquic/picoquic
           ${CMAKE_BINARY_DIR}/../picoquic/picoquic
           ../picotls/picoquic/ )
 
+find_path(Picoquic_BINLOG_DIR
+    NAMES autoqlog.h
+    HINTS ${CMAKE_SOURCE_DIR}/../picoquic/binlog
+          ${CMAKE_BINARY_DIR}/../picoquic/binlog
+          ../picotls/picoquic/ )
+
 set(Picoquic_HINTS ${CMAKE_BINARY_DIR}/../picoquic ../picoquic)
 
 find_library(Picoquic_CORE_LIBRARY picoquic-core HINTS ${Picoquic_HINTS})
+find_library(Picoquic_LOG_LIBRARY picoquic-log HINTS ${Picoquic_HINTS})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set Picoquic_FOUND to TRUE
@@ -20,7 +28,9 @@ find_package_handle_standard_args(Picoquic REQUIRED_VARS
 if(Picoquic_FOUND)
     set(Picoquic_LIBRARIES
         ${Picoquic_CORE_LIBRARY} )
-    set(Picoquic_INCLUDE_DIRS ${Picoquic_INCLUDE_DIR})
+    set(Picoquic_INCLUDE_DIRS
+        ${Picoquic_INCLUDE_DIR}
+        ${Picoquic_BINLOG_DIR})
 endif()
 
 mark_as_advanced(Picoquic_LIBRARIES Picoquic_INCLUDE_DIRS)
