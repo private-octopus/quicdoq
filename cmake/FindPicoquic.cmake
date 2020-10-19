57220dd5358ca35ea40602ad6523ae5f00a7422e
Christian Huitema <huitema@huitema.net>
2020-10-19 13:00:51 -0700

    Add ref to log library

--- cmake/FindPicoquic.cmake
+++ cmake/FindPicoquic.cmake
@@ -27,7 +27,8 @@ find_package_handle_standard_args(Picoquic REQUIRED_VARS
 
 if(Picoquic_FOUND)
     set(Picoquic_LIBRARIES
-        ${Picoquic_CORE_LIBRARY} )
+        ${Picoquic_CORE_LIBRARY}
+        ${Picoquic_LOG_LIBRARY})
     set(Picoquic_INCLUDE_DIRS
         ${Picoquic_INCLUDE_DIR}
         ${Picoquic_BINLOG_DIR})
